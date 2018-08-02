d5c20002ba876a0e0da1d1475234f292e978b44c
Eduardo Vicente Soares Silva <eduardovsoares@hotmail.com>
2018-08-02 20:13:44 -0300

    Numero massa

--- Aula0208/Projeto0208/Program.cs
+++ Aula0208/Projeto0208/Program.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projeto0208
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Projeto0208 { 
 
-            Buceta
+    class Program { 
+    
+        static void Main(string[] args) {
+
+            int n;
+
+            Console.WriteLine("Digite um número massa aí: ");
+                n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello MuthaFucking World, você digitou o número: " + n);
+            Console.ReadKey();
+
            
 
         }
