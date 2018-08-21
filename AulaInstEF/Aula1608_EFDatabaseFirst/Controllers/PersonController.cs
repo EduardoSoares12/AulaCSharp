4e27bd2f6d8570fcbf4694ad8918c7b7099489da
Eduardo Vicente Soares Silva <eduardovicentesoares@gmail.com>
2018-08-21 20:12:36 -0300

    Alterações Person Controller

---
+++ AulaInstEF/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608_EFDatabaseFirst.Controllers
+{
+    class PersonController
+    {
+        void Inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+
+        List<Person> ListarTodosPerson()
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+        }
+
+        Person BuscarPorId(int id)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.Find(id);
+        }
+
+        void Excluir(int id)
+        {
+            Person pExcluir = BuscarPorId(id);
+            if(pExcluir != null)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+        }
+
+        void Editar(int id, Person novoDadosPerson)
+        {
+            Person personAntigo = BuscarPorId(id);
+            if(personAntigo != null)
+            {
+                personAntigo.FirstName = novoDadosPerson.FirstName;
+                personAntigo.LastName = novoDadosPerson.LastName;
+                personAntigo.Title = novoDadosPerson.Title;
+
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+
+                contexto.Entry(personAntigo).State =
+                    System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+
+        List<Person> PesquisaPorFirstName(string firstName){
+
+           //LINQ
+           AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            var lista = from p in contexto.Person
+                        select p;//SELECT * FROM Person
+
+            var lista = from p in contexto.Person
+                        where p.FirstName == firstName
+                        select p;
+
+            return lista.ToList();
+        }
+
+    }
+}
---
+++ AulaInstEF/Aula1608_EFDatabaseFirst/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Aula1608_EFDatabaseFirst.Controllers { 
+    public class ProductController
+    {
+	    public ProductController()
+	    {
+            void Inserir(Product p)
+            {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Product.Add(p);
+                contexto.SaveChanges();
+            }
+
+	    }
+    }
+}
